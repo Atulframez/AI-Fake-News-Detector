7d3164fb524d3f68e00fe49cd2515d38b71c71e9
Atulanand09 <atulanand092004@gmail.com>
2026-05-04 22:44:10 +0530

    feat: add Accept-Encoding, Connection and Cache-Control headers to scraper

--- model/scraper.py
+++ model/scraper.py
@@ -30,6 +30,9 @@ HEADERS = {
     ),
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache',
 }
 
 TIMEOUT = 15  # seconds
